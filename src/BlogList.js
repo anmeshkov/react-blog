6e16ca2f1365fcc3f1da6e9f8dc91eb3b9d4c6dc
Andrew Meshkov <meshkov89@gmail.com>
2024-07-10 15:02:18 +0300

    feat: Параметры для маршрутов. route parameters

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,20 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ posts }) => {
-	return (
-		<div className='blog'>
-			{posts.map((post) => (
-				<div className='post-preview' key={post.id}>
-					<h2>{post.title}</h2>
-					<p>{post.author}</p>
-					{/* <button className='btn-delete'>Delete</button> */}
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div className="blog">
+      {posts.map((post) => (
+        <div className="post-preview" key={post.id}>
+          <Link to={`/blogs/${post.id}`}>
+            <h2>{post.title}</h2>
+            <p>{post.author}</p>
+          </Link>
+
+          {/* <button className='btn-delete'>Delete</button> */}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default BlogList;
