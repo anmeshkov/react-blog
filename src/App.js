71865ac5348555520e4857d5c2d2a8872eabfe9d
Andrew Meshkov <meshkov89@gmail.com>
2024-07-10 14:11:53 +0300

    feat: Подключение react-router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import Navbar from './Navbar';
-import Home from './Home';
-
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-	return (
-		<div className='app'>
-			<Navbar />
-			<main className='main'>
-				<Home />
-			</main>
-		</div>
-	);
+  return (
+    <Router>
+      <div className="app">
+        <Navbar />
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
