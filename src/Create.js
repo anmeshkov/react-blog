0d3cdff08b355fb71ba418ba933e5844a2b4f172
Andrew Meshkov <meshkov89@gmail.com>
2024-07-10 14:39:17 +0300

    feat: cleanup функция внутри useEffect для остановки fetch запроса

---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+  return ( 
+    <div className="create">
+      <p>New POST</p>
+    </div>
+   );
+}
+ 
+export default Create;
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,33 +1,46 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 const useFetch = (url) => {
-    const [data, setData] = useState(null)
-    const [isLoading, setLoading] = useState(true)
-    const [error, setError]= useState(null);
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(()=>{
-        console.log('useEffect run');
+  useEffect(() => {
+    console.log("useEffect run");
 
-        setTimeout(()=>{
-            fetch(url).then((res) => {
-                if (res.ok !== true){
-                    throw Error('Could not fetch the data from this resource');
-                }
-                return res.json()
-            }).then((data) => {
-                console.log(data);
-                setData(data)
-                setLoading(false)
-                setError(null)
-            }).catch((err)=>{
-                setError(err.message)
-                setLoading(false)
-            })
-        }, 1000)
+    const abortCont = new AbortController();
 
-    }, [])
+    setTimeout(() => {
+      fetch(url, { signal: abortCont.signal })
+        .then((res) => {
+          if (res.ok !== true) {
+            throw Error("Could not fetch the data from this resource");
+          }
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          setData(data);
+          setLoading(false);
+          setError(null);
+        })
+        .catch((err) => {
+          if ((err.name = "AbortError")) {
+            console.log("Fetch aborted");
+          } else {
+            setError(err.message);
+            setLoading(false);
+          }
+        });
+    }, 1000);
 
-    return {data, isLoading, error}
-}
+    return () => {
+      console.log("useEffect cleanup");
+      abortCont.abort(); // abort the fetch request if component unmounts before it resolves
+    };
+  }, []);
+
+  return { data, isLoading, error };
+};
 
 export default useFetch;
