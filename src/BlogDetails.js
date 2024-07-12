5212a792ebe89129e4a337475a64107bf18975f6
Andrew Meshkov <meshkov89@gmail.com>
2024-07-12 11:15:57 +0300

    feat: добавлено удаление поста

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,37 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+
 
 const BlogDetails = (params) => {
-const {id} = useParams()
-  
-  return ( 
+  const { id } = useParams();
+  const { data: blog, isLoading, error } = useFetch(
+    "http://localhost:8000/posts/" + id
+  );
+  const navigate = useNavigate()
+
+  const deletePost = () => {
+    fetch('http://localhost:8000/posts/' + blog.id, {
+      method: 'DELETE'
+    }).then(
+      navigate('/')
+    )
+  }
+
+  return (
     <div className="blog-details">
-      <h2>BlogDetails - {id}</h2>
+      {isLoading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p className="author">Written by: {blog.author}</p>
+          <div className="blog-body">{blog.body}</div>
+          <button onClick={deletePost} className="btn-delete">Delete</button>
+        </article>
+      )}
     </div>
-   );
-}
- 
+  );
+};
+
 export default BlogDetails;
